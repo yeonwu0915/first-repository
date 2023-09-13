92024520b2af259c5d1c6b08dca82bf424982246
yeonwu0915 <jyw7473@gmail.com>
2023-09-13 15:20:09 +0900

    Update hello_world.py

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,10 @@
 def print_hello():
     print("Hello World!!")
 
+def print_kit():
+    print("kumoh national institute of technology.")
+
 if __name__ == '__main__':
     print_hello()
+    print_kit()
     
